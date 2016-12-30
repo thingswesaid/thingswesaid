cd16f27588691a056645592cb073b8c065af0b83
dicristomanuel <manuel.dicristo@raise.com>
2016-12-30 15:59:35 -0600

    [second-section] evo-side

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function checkElementInViewport(el) {
+function checkElementInViewport(el, margin = 0) {
   var top = el.offsetTop;
   var left = el.offsetLeft;
   var width = el.offsetWidth;
@@ -12,30 +12,43 @@ function checkElementInViewport(el) {
   }
 
   return (
-    top >= window.pageYOffset + 100 &&
+    top >= window.pageYOffset + margin &&
     left >= window.pageXOffset &&
     (top + height) <= (window.pageYOffset + window.innerHeight) &&
     (left + width) <= (window.pageXOffset + window.innerWidth)
   );
 };
 
-var thingswesaidTitle = document.querySelector('.intro-animation');
+var mainTitle = document.querySelector('.intro-animation');
 var firstSectionDeer = document.querySelector('.first-section-deer');
 var arrowDown = document.querySelector('.arrow-down');
+var evoSide = document.querySelector('.evo-side');
+var voices = document.querySelector('.lombok');
 
-var isElementInViewport;
-var memoizeElementInViewport = isElementInViewport;
+var memoMainTitleInViewport;
+var memoEvoSideInViewport = false;
 
 window.addEventListener("scroll", function() {
   arrowDown.classList.remove('fadein-with-delay');
   arrowDown.classList.add('fadeout-fast');
 
-  var isInViewAfterScrolling = checkElementInViewport(thingswesaidTitle);
-  if (isInViewAfterScrolling !== memoizeElementInViewport) {
-    var addClass = isInViewAfterScrolling ? 'fadein' : 'fadeout';
+  var mainTitleInViewScrolling = checkElementInViewport(mainTitle, 100);
+
+  if (mainTitleInViewScrolling !== memoMainTitleInViewport) {
+    var addClass = mainTitleInViewScrolling ? 'fadein' : 'fadeout';
     var removeClass = addClass === 'fadein' ? 'fadeout' : 'fadein';
     firstSectionDeer.classList.remove(removeClass);
     firstSectionDeer.classList.add(addClass);
-    memoizeElementInViewport = isInViewAfterScrolling;
+    memoMainTitleInViewport = mainTitleInViewScrolling;
+  }
+
+  if (!mainTitleInViewScrolling !== memoEvoSideInViewport && checkElementInViewport(voices)) {
+    evoSide.classList.remove('fadeout');
+    evoSide.classList.add('fadein-with-delay');
+    memoEvoSideInViewport = !mainTitleInViewScrolling;
+  } else if(mainTitleInViewScrolling && evoSide.classList.contains('fadein-with-delay')) {
+    evoSide.classList.remove('fadein-with-delay');
+    evoSide.classList.add('fadeout');
+    memoEvoSideInViewport = !mainTitleInViewScrolling;
   }
 });
